5f0b3934e8b0a57917f79e3234cd7dd607ef2e62
ThaerHussein <tselawe706@gmail.com>
2024-03-25 13:26:32 +0300

    Class 07

---
+++ Class-07/Demo/coffee.js
@@ -0,0 +1,21 @@
+'use strict';
+
+function Drink(name, ingredients, isCold, isHot, price) {
+    this.name = name;
+    this.ingredients = ingredients;
+    this.isCold = isCold;
+    this.isHot = isHot;
+    this.price = price;
+}
+
+Drink.prototype.render = function() {
+    document.write(`${this.name} was added to the coffee menu  \r`);
+}
+
+let espresso = new Drink('espresso', ['coffee','water'],false, true,"5$");
+let latte = new Drink('latte', ['coffee','water','milk'],true, true,"5$");
+
+espresso.render();
+latte.render();
+
+console.log(espresso)
